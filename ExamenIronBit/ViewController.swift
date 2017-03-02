79d755980e7741867f96b5f320a3a0572379edbe
Gandhi Mena Salas <gandhimena@MacBook-Pro-de-Gandhi.local>
2017-03-02 16:02:41 -0600

    first commit

---
+++ ExamenIronBit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExamenIronBit
+//
+//  Created by Gandhi Mena Salas on 02/03/17.
+//  Copyright © 2017 Gandhi Mena Salas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
